31dd8b6a229efc5a464d284cbc8489835ff96f72
Jon Manock <jonmanock@Jons-MacBook-Pro.local>
2019-06-01 16:54:58 -0400

    added brit about

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -4,7 +4,16 @@ const About = () =>{
   return(
     <div style={{marginTop:150}} id='about'>
       <h2>About</h2>
-      <h3>Brittani Fernandez</h3>
+      <p></p>
+      <h3>Brittani Fernandez ~ Owner</h3>
+      <p>Brittani has been involved and worked in the beauty and entertainment industries for over 20 years. She started doing make-up in theater in middle school which ignitted her path to a career as a professional Make-Up Artist. She attended Joe Blasco School for Make-Up Artistry in Orlando in 2005.</p>
+      <p>In 2013, she began training at Hollywood Instute of Beauty Careers for a license in Skin Care. Agter completing the Skin Care program she extended her education by also earning a total of 3 licenses which include:</p>
+      <ul>
+        <li>Cosmetology</li>
+        <li>Skin Care</li>
+        <li>Nail Specialist</li>
+      </ul>
+      <p>Brittani has been working as a progessional stylist at Salon Retro since 2015 and has since become the owner.</p>
     </div>
   );
 };
